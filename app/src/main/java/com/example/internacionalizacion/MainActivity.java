6797f142254dd06215f90cbeb8e199219b3b7d0b
krisanbo <krisanbo@gmail.com>
2023-04-19 11:20:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/internacionalizacion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.internacionalizacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
